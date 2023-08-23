4f9d49995b5fb56a88d9cbca761eaa91e696a824
InomjonDev <inomjonismanaliev@gmail.com>
2023-08-23 14:25:52 +0500

    Cart

--- src/static/index.js
+++ src/static/index.js
@@ -1,39 +1,42 @@
 export const ProductsData = [
-    {
-      name: "Bread",
-      price: 15,
-      category: "tort",
-      url: [
-        "https://clenu-store-demo.myshopify.com/cdn/shop/products/pro1_79599500-9bf6-446e-b6f4-6833b9c2efd6.jpg?v=1676536072",
-        "https://clenu-store-demo.myshopify.com/cdn/shop/products/pro4_65d08cc9-3bc1-4335-ad13-d53c671287b8.jpg?v=1676536072",
-      ],
-    },
-    {
-      name: "Ergonomic Granite Chair",
-      price: 12,
-      category: "tort",
-      url: [
-        "https://clenu-store-demo.myshopify.com/cdn/shop/products/product48.jpg?v=1676536053",
-        "https://clenu-store-demo.myshopify.com/cdn/shop/products/product47.jpg?v=1676536053",
-      ],
-    },
-    {
-      name: "Custard Bun",
-      price: 16,
-      category: "pirojniy",
-      url: [
-        "https://clenu-store-demo.myshopify.com/cdn/shop/products/product19.jpg?v=1676536050",
-        "https://clenu-store-demo.myshopify.com/cdn/shop/products/product22.jpg?v=1676536050",
-      ],
-    },
-    {
-      name: "Cupcake Queen",
-      price: 20,
-      category: "biscuite",
-      url: [
-        "https://clenu-store-demo.myshopify.com/cdn/shop/products/product1.jpg?v=1676536045",
-        "https://clenu-store-demo.myshopify.com/cdn/shop/products/product4.jpg?v=1676536045",
-      ],
-    },
-  ];
-  
+  {
+    id: "pro-1",
+    name: "Bread",
+    price: 15,
+    category: "tort",
+    url: [
+      "https://clenu-store-demo.myshopify.com/cdn/shop/products/pro1_79599500-9bf6-446e-b6f4-6833b9c2efd6.jpg?v=1676536072",
+      "https://clenu-store-demo.myshopify.com/cdn/shop/products/pro4_65d08cc9-3bc1-4335-ad13-d53c671287b8.jpg?v=1676536072",
+    ],
+  },
+  {
+    id: "pro-2",
+    name: "Ergonomic Granite Chair",
+    price: 12,
+    category: "tort",
+    url: [
+      "https://clenu-store-demo.myshopify.com/cdn/shop/products/product48.jpg?v=1676536053",
+      "https://clenu-store-demo.myshopify.com/cdn/shop/products/product47.jpg?v=1676536053",
+    ],
+  },
+  {
+    id: "pro-3",
+    name: "Custard Bun",
+    price: 16,
+    category: "pirojniy",
+    url: [
+      "https://clenu-store-demo.mysho pify.com/cdn/shop/products/product19.jpg?v=1676536050",
+      "https://clenu-store-demo.myshopify.com/cdn/shop/products/product22.jpg?v=1676536050",
+    ],
+  },
+  {
+    id: "pro-4",
+    name: "Cupcake Queen",
+    price: 20,
+    category: "biscuite",
+    url: [
+      "https://clenu-store-demo.myshopify.com/cdn/shop/products/product1.jpg?v=1676536045",
+      "https://clenu-store-demo.myshopify.com/cdn/shop/products/product4.jpg?v=1676536045",
+    ],
+  },
+];
