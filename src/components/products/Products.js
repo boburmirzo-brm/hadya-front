afc440b123680e62b8cf02021c659bc684c570a5
rahmatillokh <webcoder292@gmail.com>
2023-08-23 14:01:49 +0500

    container added for products

--- src/components/products/Products.js
+++ src/components/products/Products.js
@@ -11,38 +11,40 @@ function Products() {
   const [hoveredItem, setHoveredItem] = useState(null);
 
   return (
-    <div className="products">
-      {ProductsData?.map((item, id) => (
-        <div
-          key={item.name}
-          className="product"
-          onMouseEnter={() => setHoveredItem(item)}
-          onMouseLeave={() => setHoveredItem(null)}
-        >
-          <img
-            src={hoveredItem === item ? item.url[1] : item.url[0]}
-            alt={item.name}
-            className="product-image"
-          />
-          <div className="action__wrapper">
-            <div className="actions">
-              <div className="cart">
-                <AiOutlineShoppingCart />
-              </div>
-              <div className="search">
-                <AiOutlineSearch />
-              </div>
-              <div className="heart">
-                <AiOutlineHeart />
+    <div className="container">
+      <div className="products">
+        {ProductsData?.map((item, id) => (
+          <div
+            key={item.name}
+            className="product"
+            onMouseEnter={() => setHoveredItem(item)}
+            onMouseLeave={() => setHoveredItem(null)}
+          >
+            <img
+              src={hoveredItem === item ? item.url[1] : item.url[0]}
+              alt={item.name}
+              className="product-image"
+            />
+            <div className="action__wrapper">
+              <div className="actions">
+                <div className="cart">
+                  <AiOutlineShoppingCart />
+                </div>
+                <div className="search">
+                  <AiOutlineSearch />
+                </div>
+                <div className="heart">
+                  <AiOutlineHeart />
+                </div>
               </div>
             </div>
+            <div>
+              <h5>{item.name}</h5>
+              <h4>${item.price}.00</h4>
+            </div>
           </div>
-          <div>
-            <h5>{item.name}</h5>
-            <h4>${item.price}.00</h4>
-          </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 }
