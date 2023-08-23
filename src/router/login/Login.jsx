a0d570f0cbb1e404309ec8cb3dfd9846e4ab1094
murodjon99 <murodjonmirbabaev04@gmail.com>
2023-08-23 02:52:18 -0700

    salom

--- src/router/login/Login.jsx
+++ src/router/login/Login.jsx
@@ -2,46 +2,39 @@ import React from 'react'
 import profile from "../../assets/download.png";
 import email from "../../assets/email.jpg";
 import pass from "../../assets/pass.png";
+import { Link } from "react-router-dom"
 import "./Login.css"
 function Login() {
-  return (
-    <div className="main_containter">
-    <div className="sub-main">
-      <div>
-        <div className="imgs">
-          <div className="container-image">
-            <img src={profile} alt="profile" className="profile"/>
-
-          </div>
-
-
+    return (
+        <div className="main_containter">
+            <div className="sub-main">
+                <div>
+                    <div className="imgs">
+                        <div className="container-image">
+                            <img src={profile} alt="profile" className="profile" />
+                        </div>
+                    </div>
+                    <div>
+                        <h1>Login Page</h1>
+                        <div>
+                            <img src={email} alt="email" className="email" />
+                            <input type="text" placeholder="username" className="name" />
+                        </div>
+                        <div className="second-input">
+                            <img src={pass} alt="pass" className="email" />
+                            <input type="password" placeholder="password" className="name" />
+                        </div>
+                        <div className="login-button">
+                            <button>Login</button>
+                        </div>
+                        <Link to={"/"}><p className="link">
+                            home
+                        </p></Link>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div>
-          <h1>Login Page</h1>
-          <div>
-            <img src={email} alt="email" className="email"/>
-            <input type="text" placeholder="username" className="name"/>
-          </div>
-          <div className="second-input">
-            <img src={pass} alt="pass" className="email"/>
-            <input type="password" placeholder="password" className="name"/>
-          </div>
-         <div className="login-button">
-         <button>Login</button>
-         </div>
-          
-           <p className="link">
-             <a href="#">Forgot password ?</a> Or<a href="#">Sign Up</a>
-           </p>
-          
-
-        </div>
-      </div>
-      
-
-    </div>
-   </div>
-  )
+    )
 }
 
 export default Login
