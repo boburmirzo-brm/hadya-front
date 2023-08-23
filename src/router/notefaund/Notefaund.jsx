f6327b3bc3f231df998e33418229e0e67f576a48
murodjon99 <murodjonmirbabaev04@gmail.com>
2023-08-23 01:33:02 -0700

    salom

--- src/router/notefaund/Notefaund.jsx
+++ src/router/notefaund/Notefaund.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import "./Notefaund.css"
-import not from "../../assets/404.png"
+import not from "../../assets/404.png" 
 export default function Notefaund() {
   return (
     <div className='not__faund'>
-        <p>Ой, кажется я что-то понажимала…</p>
+        <p className='p_Tree'>Ой, кажется я что-то понажимала…</p>
         <img src={not} alt="" />
         <p className='p_One'>Пока я тут разбираюсь с ошибкой, по номеру 78 113 40 40 вам помогут.</p>
         <p className='p_Two'>НА ГЛАВНУЮ</p>
