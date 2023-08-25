aad96f23e8157c0317cab54def1f08eddb918a51
murodjon99 <murodjonmirbabaev04@gmail.com>
2023-08-25 03:19:44 -0700

    salom

--- src/components/empty/Empty.jsx
+++ src/components/empty/Empty.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import "./Empty.css"
+import rasim from "../../assets/banner_img_one.webp"
 import {Link} from "react-router-dom"
 function Empty({title}) {
   return (
     <div className="empty__container">
+      <img src={rasim} />
     <h3>{title}da hozircha mahsulot yo'q</h3>
     <p>
       Bosh sahifadagi to’plamlardan boshlang yoki kerakli mahsulotni qidiruv
