04b262fcd0d3fee31b95265f2fb117f677bd9337
rahmatillokh <webcoder292@gmail.com>
2023-09-06 15:38:34 +0500

    toasts added

--- src/router/admin/create-product/CreateProduct.js
+++ src/router/admin/create-product/CreateProduct.js
@@ -3,6 +3,8 @@ import "./CreateProduct.css";
 import { category } from "../../../static";
 import axios from "../../../api";
 
+import { toast } from "react-toastify";
+
 const initialState = {
   name: "",
   category: "",
@@ -12,25 +14,34 @@ const initialState = {
 function CreateProduct() {
   const [product, setProduct] = useState(initialState);
   const [files, setFiles] = useState("");
-  const handleSubmit = e =>{
-    e.preventDefault()
-    let newProduct = new FormData()
-    newProduct.append("name", product.name)
-    newProduct.append("desc", product.desc)
-    newProduct.append("price", product.price)
-    newProduct.append("category", product.category)
-    Array.from(files).forEach(i => {
-      newProduct.append("rasmlar", i, i.name)
-    })
-    axios.post("/create/product", newProduct)
-      .then(res=> {
-        console.log(res)
-        setProduct(initialState)
-        setFiles("")
+
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    let newProduct = new FormData();
+    newProduct.append("name", product.name);
+    newProduct.append("desc", product.desc);
+    newProduct.append("price", product.price);
+    newProduct.append("category", product.category);
+    Array.from(files).forEach((i) => {
+      newProduct.append("rasmlar", i, i.name);
+    });
+    axios
+      .post("/create/product", newProduct)
+      .then((res) => {
+        console.log(res);
+        setProduct(initialState);
+        setFiles("");
+        toast.success("Mahsulot qo'shildi!");
       })
-      .catch(err=> console.log(err))
-      
-  }
+      .catch((err) => {
+        console.log(err);
+        toast.error("Server error");
+      })
+      .finally(() => setLoading(false));
+  };
   return (
     <div className="create__product">
       <h3>Mahsulot qo'shish uchun formani to'ldiring!</h3>
@@ -45,6 +56,7 @@ function CreateProduct() {
             type="text"
             className="form-control"
             placeholder="Mahsulot nomi"
+            disabled={loading}
           />
         </div>
         <div>
@@ -57,17 +69,19 @@ function CreateProduct() {
             type="number"
             className="form-control"
             placeholder="Mahsulot narhi"
+            disabled={loading}
           />
         </div>
 
         <div>
           <input
-            onChange={e => setFiles(e.target.files)}
+            onChange={(e) => setFiles(e.target.files)}
             required
             type="file"
             multiple
             className="form-control"
             placeholder="Mahsulot rasmi"
+            disabled={loading}
           />
         </div>
         <div>
@@ -79,6 +93,7 @@ function CreateProduct() {
             type="text"
             className="form-control"
             placeholder="Mahsulot Tavsifi"
+            disabled={loading}
           />
         </div>
         <div>
@@ -90,6 +105,7 @@ function CreateProduct() {
             className="form-control"
             name=""
             id=""
+            disabled={loading}
           >
             <option value="" disabled>
               Tanlang
@@ -101,7 +117,9 @@ function CreateProduct() {
             ))}
           </select>
         </div>
-        <button>Jonatish</button>
+        <button disabled={loading}>
+          {loading ? "Yuklanmoqda" : "Jo'natish"}
+        </button>
       </form>
     </div>
   );
