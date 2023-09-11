21c533357789b8190cbb95f4d28d7c177ffc9de8
InomjonDev <inomjonismanaliev@gmail.com>
2023-09-11 17:36:33 +0500

    Navbar cart length

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,62 +1,73 @@
-import React, {useState} from 'react'
-import { 
- AiOutlineUser, 
- AiOutlineHeart, 
- AiOutlineShoppingCart, 
- AiOutlineMenu,
- AiOutlineHome } from "react-icons/ai"
-import {Link, useLocation} from "react-router-dom"
-import "./Navbar.css"
-import logo from "../../assets/hadya-logo.png"
-import Sidebar from '../sidebar/Sidebar'
+import React, { useState } from "react";
+import {
+	AiOutlineUser,
+	AiOutlineHeart,
+	AiOutlineShoppingCart,
+	AiOutlineMenu,
+	AiOutlineHome
+} from "react-icons/ai";
+import { Link, useLocation } from "react-router-dom";
+import "./Navbar.css";
+import logo from "../../assets/hadya-logo.png";
+import Sidebar from "../sidebar/Sidebar";
+import { useSelector } from "react-redux";
 
 function Navbar() {
-    const {pathname} = useLocation()
-    const [show, setShow] = useState(false  )
-    document.body.style.overflow = show ? "hidden" : "auto"
+	const { pathname } = useLocation();
+	const [show, setShow] = useState(false);
+	const cart = useSelector(s => s.cart.value);
 
-    if(pathname.includes("login")){
-        return <></>
-    }
-    if(pathname.includes("admin")){
-        return <></>
-    }
-    return (
-            <>
-        <div className='navbar'>
-        <div className='container navbar__main'>
-            <Link to={"/"} className="nav__logo"><img src={logo} alt="" /> </Link>
-            <button className='nav__btn' onClick={()=> setShow(true)}><AiOutlineMenu/></button>
-            <div className="item1">
-            <ul>
-              <Link to={"/about"}>
-                <li>Biz Haqimizda</li>
-              </Link>
-              <Link to={"/branches"}>
-              <li>Filyallarimiz</li>
-              </Link>
-            </ul>
-            </div>
+	document.body.style.overflow = show ? "hidden" : "auto";
 
-            <ul className="nav__collection">
-                <Link to={"/"} className="nav__item">
-                    <AiOutlineHome />
-                </Link>
-                <Link to={"/admin/create-product"} className="nav__item">
-                    <AiOutlineUser />
-                </Link>
-                <Link to={"/wishlist"} className="nav__item">
-                    <AiOutlineHeart />
-                </Link>
-                <Link to={"/cart"} className="nav__item">
-                    <AiOutlineShoppingCart />
-                </Link>
-            </ul>
-        </div>
-        </div>
-        <Sidebar show={show} setShow={setShow}/> 
-</>
-    )
+	if (pathname.includes("login")) {
+		return <></>;
+	}
+	if (pathname.includes("admin")) {
+		return <></>;
+	}
+
+	console.log(cart);
+	return (
+		<>
+			<div className="navbar">
+				<div className="container navbar__main">
+					<Link to={"/"} className="nav__logo">
+						<img src={logo} alt="" />{" "}
+					</Link>
+					<button className="nav__btn" onClick={() => setShow(true)}>
+						<AiOutlineMenu />
+					</button>
+					<div className="item1">
+						<ul>
+							<Link to={"/about"}>
+								<li>Biz Haqimizda</li>
+							</Link>
+							<Link to={"/branches"}>
+								<li>Filyallarimiz</li>
+							</Link>
+						</ul>
+					</div>
+
+					<ul className="nav__collection">
+						<Link to={"/"} className="nav__item">
+							<AiOutlineHome />
+						</Link>
+						<Link to={"/admin/create-product"} className="nav__item">
+							<AiOutlineUser />
+						</Link>
+						<Link to={"/wishlist"} className="nav__item">
+							<AiOutlineHeart />
+						</Link>
+						<Link to={"/cart"} className="nav__item">
+							<span>{cart.length}</span>
+							<AiOutlineShoppingCart />
+						</Link>
+					</ul>
+				</div>
+			</div>
+			<Sidebar show={show} setShow={setShow} />
+		</>
+	);
 }
 
-export default Navbar
+export default Navbar;
