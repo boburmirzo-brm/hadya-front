9ff297145fd12c9e376353e1e12c6dabdb35d149
boburmirzo-brm <boburmirzo.brm@gmail.com>
2023-12-18 11:26:55 +0500

    sidebar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './Sidebar.css';
 import { Link } from 'react-router-dom';
 import Hadya from "../../assets/hadya-logo.png"
+import { category } from '../../static';
 const icons = [
   { filial: "Filiallar" },
   { haqimizda: "Biz haqimizda " },
@@ -14,13 +15,21 @@ function Sidebar({ show, setShow }) {
       <div className={`sidebar ${show ? 'show' : ''}`}>
         <div className="sidebar__content">
           <img src={Hadya} alt="" />
-          {icons.map((icon, index) => (
-            <div className="sidebar__item" key={index}> 
-              <span onClick={()=> setShow(false)}> <Link to={"/branches"}>{icon.filial}</Link> </span>
-              <font onClick={()=> setShow(false)}> <Link to={"/about"}>{icon.haqimizda}</Link> </font>
-            </div>
-          ))}
+            <div className="sidebar__item" > 
+              <Link onClick={() => setShow(false)} to={"/about"}>Biz haqimizda</Link> 
+              <Link onClick={() => setShow(false)} to={"/branches"}>Filiallar</Link> 
+              <hr  className='hr'/>
+              <b>Kategoriya</b>
+                {
+                  category?.map((item) =>
+                    <Link onClick={() => setShow(false)} to={`/products/${item.link}`} key={item.id}>
+                      <p>{item.title}</p>
+                    </Link>
+                  )
+                }
+              </div>
         </div>
+        <p className='sidebar__namtech'>© 2023 NamTech Solution Inc.</p>
       </div>
     </>
   );
