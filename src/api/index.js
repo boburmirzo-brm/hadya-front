3351e42c564957cb0efebf0198798707e0b602cd
boburmirzo-brm <boburmirzo.brm@gmail.com>
2023-12-20 19:29:00 +0500

    added new api

--- src/api/index.js
+++ src/api/index.js
@@ -3,7 +3,8 @@ import axios from "axios"
 const mainURL = axios.create({
     // baseURL: "http://localhost:8000",
     // baseURL: "https://hadya.onrender.com",
-    baseURL: "https://cheerfulexemplaryfirewall--boburmirzo-brm.repl.co",
+    // baseURL: "https://cheerfulexemplaryfirewall--boburmirzo-brm.repl.co",
+    baseURL: "https://hadyasweets.uz",
 })
 
 export default mainURL
