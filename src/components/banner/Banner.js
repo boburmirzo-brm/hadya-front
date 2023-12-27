497f9d62c17a81343a94a83e2eed73d8637cfd45
boburmirzo-brm <boburmirzo.brm@gmail.com>
2023-12-27 22:44:02 +0500

    banner

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -1,18 +1,35 @@
 import React from "react";
 import "./Banner.css";
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/swiper-bundle.css";
-import { Autoplay } from "swiper/modules";
-import img1 from "../../assets/banner006.jpg";
+// import { Swiper, SwiperSlide } from "swiper/react";
+// import "swiper/swiper-bundle.css";
+// import { Autoplay } from "swiper/modules";
+// import img1 from "../../assets/banner006.jpg";
 import img2 from "../../assets/banner008.jpg";
-import img3 from "../../assets/banner007.jpg";
-import img4 from "../../assets/banner005.jpg";
+// import img3 from "../../assets/banner007.jpg";
+// import img4 from "../../assets/banner005.jpg";
 import { Link } from "react-router-dom";
 
 function Banner() {
   return (
     <div className={`slider__container `}>
-      <Swiper
+      <div className="banner__swiper">
+
+      <div className="banner__item">
+      <img src={img2} alt="" />
+          <div className="banner__dark"></div>
+          <div className="banner__text container">
+            <h1>Hello world</h1>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Reprehenderit sapiente
+            </p>
+            <Link to={"/branches"}>
+              <button>Filiallarimiz</button>
+            </Link>
+          </div>
+      </div>
+      </div>
+      {/* <Swiper
         loop={true}
         autoplay={{
           delay: 4000,
@@ -63,7 +80,7 @@ function Banner() {
             </Link>
           </div>
         </SwiperSlide>
-      </Swiper>
+      </Swiper> */}
     </div>
   );
 }
